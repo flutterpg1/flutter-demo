cadd6ee783e8b857748f88f4dc1c08ec59239c9b
pariwesh009 <ajay2322020@outlook.com>
2020-12-06 02:13:18 +0530

    added support for response processing

--- lib/Product.dart
+++ lib/Product.dart
@@ -3,4 +3,13 @@
 class Product {
   String name;
   double price;
+  int id;
+  Product({this.name, this.price, this.id});
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      price: json['price'],
+    );
+  }
 }
