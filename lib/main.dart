361f579f4cb68a40704653cd37e7315de7b80ba5
pariwesh009 <ajay2322020@outlook.com>
2020-12-07 02:06:08 +0530

    WebView demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,196 +1,170 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dynamicWidget.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
 
-void main() {
-  runApp(SampleApp());
-}
+void main() => runApp(MaterialApp(home: WikipediaExplorer()));
 
-class SampleApp extends StatelessWidget {
-  // This widget is the root of your application.
+class WikipediaExplorer extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sample App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SampleAppPage(),
-    );
-  }
+  _WikipediaExplorerState createState() => _WikipediaExplorerState();
 }
 
-class SampleAppPage extends StatefulWidget {
-  SampleAppPage({Key key}) : super(key: key);
+class _WikipediaExplorerState extends State<WikipediaExplorer> {
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+  final Set<String> _favorites = Set<String>();
 
-  @override
-  _SampleAppPageState createState() => _SampleAppPageState();
-}
-
-class _SampleAppPageState extends State<SampleAppPage> {
-  List<DynamicWidget> dynamicList = [];
-
-  List<String> price = [];
-
-  List<String> Product = [];
-  addDynamic() {
-    // if (Product.length != 0) {
-    //   floatingIcon = new Icon(Icons.add);
-    // }
-    setState(() {});
-    if (dynamicList.length >= 10) {
-      return;
-    }
-    dynamicList.add(new DynamicWidget());
-  }
-  submitData() {
-    // floatingIcon = new Icon(Icons.arrow_back);
-    Product = [];
-    price = [];
-    dynamicList.forEach((widget) => Product.add(widget.product.text));
-    dynamicList.forEach((widget) => price.add(widget.Price.text));
-    setState(() {});
-    print(Product.length);
-    // sendData();
-  }
-
-  Widget submitButton = new Container(
-    child: new RaisedButton(
-      // onPressed:submitData,
-      child: new Padding(
-        padding: new EdgeInsets.all(16.0),
-        child: new Text('Submit Data'),
-      ),
-    ),
-  );
-  @override
-  Widget build(BuildContext context) {
-    Widget dynamicTextField = new Flexible(
-      flex: 2,
-      child: new ListView.builder(
-        itemCount: dynamicList.length,
-        itemBuilder: (_, index) => dynamicList[index],
-      ),
-    );
-    Widget result = new Flexible(
-        flex: 1,
-        child: new Card(
-          child: ListView.builder(
-            itemCount: Product.length,
-            itemBuilder: (_, index) {
-              return new Padding(
-                padding: new EdgeInsets.all(10.0),
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Container(
-                      margin: new EdgeInsets.only(left: 10.0),
-                      child: new Text(
-                          "${index + 1} : ${Product[index]}                       ${price[index]}"),
-                    ),
-                    new Divider()
-                  ],
-                ),
-              );
-            },
-          ),
-        ));
-    return new Scaffold(
-      appBar: AppBar(title: Text('Dynamic form')),
-      drawer: Drawer(child: Text('dr'),),
-        body: new Container(
-          child: new Column(children: <Widget>[
-            Product.length == 0 ? dynamicTextField : result,
-            Product.length == 0 ? submitButton : new Container(),
-          ]),
-        ),
-        floatingActionButton: new FloatingActionButton(
-            onPressed: addDynamic, child: new Icon(Icons.add)));
-  }
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fluttereee Demo 123ss',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter11 Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Wikipedia Explorer'),
+        // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
+        actions: <Widget>[
+          NavigationControls(_controller.future),
+          Menu(_controller.future, () => _favorites),
+        ],
       ),
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-          Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Enter your email',
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
+      body: WebView(
+        initialUrl: 'https://en.wikipedia.org/wiki/Kraken',
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+      ),
+      floatingActionButton: _bookmarkButton(),
+    );
+  }
+
+  _bookmarkButton() {
+    return FutureBuilder<WebViewController>(
+      future: _controller.future,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (controller.hasData) {
+          return FloatingActionButton(
+            onPressed: () async {
+              var url = await controller.data.currentUrl();
+              _favorites.add(url);
+              Scaffold.of(context).showSnackBar(
+                SnackBar(content: Text('Saved $url for later reading.')),
+              );
+            },
+            child: Icon(Icons.favorite),
+          );
+        }
+        return Container();
+      },
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  Menu(this._webViewControllerFuture, this.favoritesAccessor);
+  final Future<WebViewController> _webViewControllerFuture;
+  // TODO(efortuna): Come up with a more elegant solution for an accessor to this than a callback.
+  final Function favoritesAccessor;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (!controller.hasData) return Container();
+        return PopupMenuButton<String>(
+          onSelected: (String value) async {
+            if (value == 'Email link') {
+              var url = await controller.data.currentUrl();
+              await launch(
+                  'mailto:?subject=Check out this cool Wikipedia page&body=$url');
+            } else {
+              var newUrl = await Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return FavoritesPage(favoritesAccessor());
+              }));
+              Scaffold.of(context).removeCurrentSnackBar();
+              if (newUrl != null) controller.data.loadUrl(newUrl);
+            }
+          },
+          itemBuilder: (BuildContext context) => <PopupMenuItem<String>>[
+                const PopupMenuItem<String>(
+                  value: 'Email link',
+                  child: Text('Email link'),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Validate will return true if the form is valid, or false if
-                      // the form is invalid.
-                      if (_formKey.currentState.validate()) {
-                        // Process data.
-                      }
-                    },
-                    child: Text('Submit'),
-                  ),
+                const PopupMenuItem<String>(
+                  value: 'See Favorites',
+                  child: Text('See Favorites'),
                 ),
               ],
-            ),
-          )
-        ]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
     );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  FavoritesPage(this.favorites);
+  final Set<String> favorites;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Favorite pages')),
+      body: ListView(
+          children: favorites
+              .map((url) => ListTile(
+                  title: Text(url), onTap: () => Navigator.pop(context, url)))
+              .toList()),
+    );
+  }
+}
+
+class NavigationControls extends StatelessWidget {
+  const NavigationControls(this._webViewControllerFuture)
+      : assert(_webViewControllerFuture != null);
+
+  final Future<WebViewController> _webViewControllerFuture;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<WebViewController>(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
+        final bool webViewReady =
+            snapshot.connectionState == ConnectionState.done;
+        final WebViewController controller = snapshot.data;
+        return Row(
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: true),
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: false),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  navigate(BuildContext context, WebViewController controller,
+      {bool goBack: false}) async {
+    bool canNavigate =
+        goBack ? await controller.canGoBack() : await controller.canGoForward();
+    if (canNavigate) {
+      goBack ? controller.goBack() : controller.goForward();
+    } else {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+            content: Text("No ${goBack ? 'back' : 'forward'} history item")),
+      );
+    }
   }
 }
